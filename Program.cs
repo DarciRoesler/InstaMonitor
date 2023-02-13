0a59c804adf9b99a2dca5986e9f8b5c45e550a62
Darci Roesler <roesler.darci@gmail.com>
2023-02-13 19:24:58 -0300

    Identificação do repositório - Declaração das listas - Segmentação do HTML

--- Program.cs
+++ Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string path = @"c:\InstaMonitor";
+
+string arquivoSeguindo = path + "\\seguindo.txt";
+string arquivoSeguidores = path + "\\seguidores.txt";
+string arquivoSeguindoHtml = path + "\\seguindoHtml.txt";
+string arquivoSeguidoresHtml = path + "\\seguidoresHtml.txt";
+string arquivoNaoSeguemDeVolta = path + "\\naoSeguemDeVolta.txt";
+string arquivoNaoSigoDeVolta = path + "\\naoSigoDeVolta.txt";
+
+string seguindoHtml = File.ReadAllText(arquivoSeguindoHtml);
+string seguidoresHtml = File.ReadAllText(arquivoSeguidoresHtml);
+
+List<string> seguindo = new List<string>();
+List<string> seguidores = new List<string>();
+List<string> naoSeguemDeVolta = new List<string>();
+List<string> naoSigoDeVolta = new List<string>();
+
+string[] splitSeguindo = seguindoHtml.Split(new[] { "perfil de " }, StringSplitOptions.RemoveEmptyEntries);
+string[] splitSeguidores = seguidoresHtml.Split(new[] { "perfil de " }, StringSplitOptions.RemoveEmptyEntries);
