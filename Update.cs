51126d3900aa0d0b949782bd77d6263b0f830c01
Darci Roesler <roesler.darci@gmail.com>
2023-02-14 02:24:44 -0300

    Inseridas as classes de Usuario, Update e Status

---
+++ Update.cs
@@ -0,0 +1,8 @@
+﻿namespace InstaMonitor
+{
+    internal class Update
+    {
+        public Status Status { get; set; }
+        public DateTime DateTime { get; set; }
+    }
+}
---
+++ Usuario.cs
@@ -0,0 +1,8 @@
+﻿namespace InstaMonitor
+{
+    internal class Usuario
+    {
+        public string? Nome { get; set; }
+        public List<Update>? ListaUpdates { get; set; }
+    }
+}
